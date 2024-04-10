5e2d3b208805ab82205ecf90828bacff863ba61e
ProfGoldstein <igoldstein@siena.edu>
2024-04-09 21:44:06 -0400

    package edu.siena.csis225.SurveyResponse version
    Added package edu.siena.csis225.SurveyResponse to show how a package allows us to use different modifiers.

---
+++ src/edu/siena/csis225/SurveyResponse/SurveyResponseIterator.java
@@ -0,0 +1,49 @@
+package edu.siena.csis225.SurveyResponse;
+
+import java.util.List;
+import java.util.Iterator;
+
+/**
+ * Part 1 of Problem Set 3 - CSIS 225
+ * 
+ * SurveyResponseIterator class for use with the SurveyResponse class
+ * 
+ * @author Ira Goldstein
+ * @version Spring 2024
+ */
+
+public class SurveyResponseIterator implements Iterator<SurveyResponse> {
+
+    // Variables for iterator
+    private List<SurveyResponse> responses;
+    private int current;
+
+    /**
+     * SurveyResponseIterator constructs an iterator object.
+     * 
+     * @param List<SurveyResponse> responses, list of the survey responses.
+     */
+    public SurveyResponseIterator(List<SurveyResponse> responses) {
+        this.responses = responses;
+    }
+
+    /**
+     * Iterator hasNext method
+     * 
+     * @return boolean, returns true we have more responses
+     */
+    @Override
+    public boolean hasNext() {
+        return current < responses.size();
+    }
+
+    /**
+     * Iterator next method
+     * 
+     * @return the next SurveyResponse
+     */
+    @Override
+    public SurveyResponse next() {
+        return responses.get(current++);
+    }
+}
---
+++ src/edu/siena/csis225/SurveyResponse/TestSurveyResponse.java
@@ -0,0 +1,110 @@
+package edu.siena.csis225.SurveyResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSurveyResponse {
+
+    /**
+     * The main method to test the SurveyResponse class.
+     *
+     * @param args Does not use arguments
+     */
+    public static void main(String[] args) {
+
+        /*
+         * test cases for PS3 part1 SurveyResponse
+         *
+         */
+
+        // Create a list of testResponses
+        List<SurveyResponse> testResponses = new ArrayList<>();
+        testResponses.add(new SurveyResponse(123, "MATH", 1, "Never again!"));
+        testResponses.add(new SurveyResponse(456, "MATH", 5, "Good"));
+        testResponses.add(new SurveyResponse(789, "MATH", 4, "Fun"));
+        testResponses.add(new SurveyResponse(246, "ART", 2));
+        testResponses.add(new SurveyResponse(468, "CSIS", 5, "Amazing Class!"));
+        testResponses.add(new SurveyResponse(902, "CSIS", 3, "VERY Hard!"));
+        testResponses.add(new SurveyResponse(111, "PHY", 4, "Good class!"));
+        testResponses.add(new SurveyResponse(789, "PHY", 2, "OK"));
+        testResponses.add(new SurveyResponse(123, "ART", 5, "Love the Prof"));
+        testResponses.add(new SurveyResponse(456, "CSIS", 4, "Homework was hard"));
+
+        // Testing SurveyResponse myIterator
+        System.out.println("Testing SurveyResponse myIterator:");
+        System.out.println("Expecting ten lines of output. Fourth comment should be blank");
+        SurveyResponseIterator myIterator = new SurveyResponseIterator(testResponses);
+
+        while (myIterator.hasNext()) {
+            SurveyResponse response = myIterator.next();
+            System.out.println("Course: " + response.course + " Student ID: " + response.studentId + ", Rating: "
+                    + response.rating + ", Comment: " + response.comment);
+        }
+
+        // Testing calculateAverageRating
+        double averageRating = SurveyResponse.calculateAverageRating(testResponses);
+        System.out.println();
+        System.out.println("Expecting 3.5 Average Rating for all courses: " + averageRating);
+
+        // Testing courseFeedback
+        System.out.println();
+        System.out.println("Testing courseFeedback() for course ART:");
+        System.out.println("Expecting two lines. ART 246 2 w/o comment  and  ART 123 10 Love the Prof");
+
+        List<SurveyResponse> artFeedback = SurveyResponse.courseFeedback(testResponses, "ART");
+        myIterator = new SurveyResponseIterator(artFeedback);
+        while (myIterator.hasNext()) {
+            SurveyResponse response = myIterator.next();
+            System.out.println("Course: " + response.course + " Student ID: " + response.studentId + ", Rating: "
+                    + response.rating + ", Comment: " + response.comment);
+        }
+
+        // Testing courseFeedback for non-existant course
+        System.out.println();
+        System.out.println("Testing courseFeedback() non-existant course:");
+        System.out.println("Expecting no output");
+
+        List<SurveyResponse> xxxFeedback = SurveyResponse.courseFeedback(testResponses, "XXX");
+        myIterator = new SurveyResponseIterator(xxxFeedback);
+        System.out.println();
+        while (myIterator.hasNext()) {
+            SurveyResponse response = myIterator.next();
+            System.out.println("Course: " + response.course + " Student ID: " + response.studentId + ", Rating: "
+                    + response.rating + ", Comment: " + response.comment);
+
+        }
+
+        // test with an empty list
+        List<SurveyResponse> testResponses2 = new ArrayList<>();
+
+        // Testing SurveyResponse myIterator
+        System.out.println("Testing SurveyResponse myIterator:");
+        System.out.println("Expecting no output");
+        myIterator = new SurveyResponseIterator(testResponses2);
+
+        while (myIterator.hasNext()) {
+            SurveyResponse response = myIterator.next();
+            System.out.println("Course: " + response.course + " Student ID: " + response.studentId + ", Rating: "
+                    + response.rating + ", Comment: " + response.comment);
+        }
+
+        // Testing calculateAverageRating
+        averageRating = SurveyResponse.calculateAverageRating(testResponses2);
+        System.out.println();
+        System.out.println("Expecting output of 0.0: " + averageRating);
+
+        // Testing courseFeedback
+        System.out.println();
+        System.out.println("Testing courseFeedback() for nonexistent course ART:");
+        System.out.println("Expecting no output");
+
+        List<SurveyResponse> nullFeedback = SurveyResponse.courseFeedback(testResponses2, "ART");
+        myIterator = new SurveyResponseIterator(nullFeedback);
+        while (myIterator.hasNext()) {
+            SurveyResponse response = myIterator.next();
+            System.out.println("Course: " + response.course + " Student ID: " + response.studentId + ", Rating: "
+                    + response.rating + ", Comment: " + response.comment);
+        }
+
+    }
+}
